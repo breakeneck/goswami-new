f93e18d9b0cbc33b57c48ad23fda26b9eb3e2400
Yuri Datsenko <breakeneck@gmail.com>
2026-04-24 21:31:07 +0300

    feat(formatter): add LRC support with timestamp parsing, formatting, and database integration

--- web/goswami/urls.py
+++ web/goswami/urls.py
@@ -9,6 +9,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('lectures.urls')),
+    path('auth/', include('auth.urls')),
 ]
 
 if settings.DEBUG:
